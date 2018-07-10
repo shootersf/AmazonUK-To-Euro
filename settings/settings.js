6cbf55764da252780382f184e2e68965814fd2a7
shootersf <me@simonfarrelly.com>
2018-07-10 21:21:47 +0100

    added tipjar

--- settings/settings.js
+++ settings/settings.js
@@ -13,4 +13,11 @@ function init() {
     }
     document.getElementById("bias-number").placholder = settings.bias;
     document.getElementById("biased-rate-in-euro").textContent = settings.rate + settings.bias/100;
+    updateTipButton();
+}
+
+function updateTipButton() {
+    let src = "/images/tip.jpg";
+    let url = chrome.extension.getURL(src);
+    document.getElementById("tip-image").src = url;
 }
